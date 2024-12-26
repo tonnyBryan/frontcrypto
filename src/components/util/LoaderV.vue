d7f84761db71350af018ba5402e63c37fee736d2
tonnyBryan <126952328+tonnyBryan@users.noreply.github.com>
2024-12-26 21:55:44 +0300

    c2 1
    loader 1.1

---
+++ src/components/util/LoaderV.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="loading-wave">
+    <div class="loading-bar"></div>
+    <div class="loading-bar"></div>
+    <div class="loading-bar"></div>
+    <div class="loading-bar"></div>
+  </div>
+</template>
+
+<style scoped>
+.loading-wave {
+  width: 150px;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+}
+
+.loading-bar {
+  width: 6px;
+  height: 10px;
+  margin: 0 3px;
+  background-color: #ffc107;
+  border-radius: 5px;
+  animation: loading-wave-animation 1s ease-in-out infinite;
+}
+
+.loading-bar:nth-child(2) {
+  animation-delay: 0.1s;
+}
+
+.loading-bar:nth-child(3) {
+  animation-delay: 0.2s;
+}
+
+.loading-bar:nth-child(4) {
+  animation-delay: 0.3s;
+}
+
+@keyframes loading-wave-animation {
+  0% {
+    height: 7px;
+  }
+
+  50% {
+    height: 25px;
+  }
+
+  100% {
+    height: 7px;
+  }
+}
+</style>
