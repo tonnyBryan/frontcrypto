b93b9152c6ae968c2c055a27b9a1ec7eb8afc3b9
VoaharyMihaja <155955196+VoaharyMihaja@users.noreply.github.com>
2024-12-26 21:57:41 +0300

    ajout vue
    J'ai ajouter les vue suivantes:
    achatCrypto, transactionCrypto, UserCrypto (voir les crypto que l'utilisateur a), venteCrypto.

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,13 @@ import NotFoundView from '@/views/404Page.vue'
 import HomeView from '@/components/main/HomeView.vue'
 import CryptoChart from '@/components/main/CryptoChart.vue'
 
+// modification Voahary
+import AchatCrypto from '@/components/main/AchatCrypto.vue'
+import VenteCrypto from '@/components/main/VenteCrypto.vue'
+import UserCrypto from '@/components/main/UserCrypto.vue'
+
+// end of modification import Voahary
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -54,6 +61,15 @@ const router = createRouter({
       name: 'NotFound',
       component: NotFoundView,
     },
+
+    // modification Voahary
+
+    { path: "/achat", name: "AchatCrypto", component: AchatCrypto },
+    { path: "/vente", name: "VenteCrypto", component: VenteCrypto },
+    { path: '/user-crypto', name: 'UserCrypto', component: UserCrypto },
+    { path: '/crypto-transaction', name: 'CryptoTransaction', component: CryptoTransaction },
+
+    // End of modification chemin Voahary
   ],
 })
 
